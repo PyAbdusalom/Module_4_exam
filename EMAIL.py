a81dce43fc0d81bd41ecd7f236bf586fbb46916e
PyAbdusalom <abdusalomabduvaliyev07@gmail.com>
2023-06-08 17:09:20 +0500

    commit

---
+++ EMAIL.py
@@ -0,0 +1,23 @@
+import smtplib
+
+text1 = "ovbmixoksmpivxoy"
+text2 = "abdusalomabduvaliyev07@gmail.com"
+
+password = text1
+sender = text2
+server = "smtp.gmail.com"
+port = 465
+receiver = "absaitovdev@gmail.com"
+message = """
+From: {}
+To: {}
+subject:
+    https://github.com/PyAbdusalom/Module_4_exam.git
+    
+    docker pull pyabdusalom/4_module_exam_img:latest
+""".format(sender, receiver)
+
+with smtplib.SMTP_SSL(server, port) as server:
+    server.login(sender, password)
+    server.sendmail(sender, receiver, message)
+    print("Sending email")
---
+++ multiThreading.py
@@ -0,0 +1,32 @@
+import threading
+from datetime import timedelta
+
+import httpx
+import redis
+
+
+def thread(name):
+    connection = redis.Redis(host="localhost", port=6379, decode_responses=True)
+    response = httpx.get(f"https://{name}")
+    data = response.content
+    connection.set(name=name, value=data, ex=timedelta(seconds=60))
+
+
+oqim1 = threading.Thread(target=thread, args=('kun.uz',))
+oqim2 = threading.Thread(target=thread, args=("daryo.uz",))
+oqim3 = threading.Thread(target=thread, args=("qalampir.uz",))
+oqim4 = threading.Thread(target=thread, args=("leetcode.com",))
+oqim5 = threading.Thread(target=thread, args=("atto.uz/uz",))
+
+oqim1.start()
+oqim2.start()
+oqim3.start()
+oqim4.start()
+oqim5.start()
+
+oqim1.join()
+oqim2.join()
+oqim3.join()
+oqim4.join()
+oqim5.join()
+
