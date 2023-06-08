1f80070715c613b886b0f288db69cfc742458c10
PyAbdusalom <abdusalomabduvaliyev07@gmail.com>
2023-06-08 16:17:05 +0500

    Initial commit

---
+++ Telegram_bot.py
@@ -0,0 +1,35 @@
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+
+API_TOKEN = os.getenv("TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot=bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message, state: FSMContext):
+    await message.reply(f"Assalomu alaykum {message.from_user.full_name} 😊")
+    await message.answer("Text Kiriting")
+    await state.set_state("text")
+
+
+@dp.message_handler(state="text")
+async def echo(message: types.Message, state: FSMContext):
+    All_vowels = "aeiouAEIOU"
+    summa = sum([1 for i in message.text if i in All_vowels])
+    if summa >= 5:
+        await message.answer("Text unlilar soni 5 dan oshib ketti. Qayta kiriting")
+        await state.set_state("text")
+    else:
+        await state.set_state("text")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
